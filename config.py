084fe266e65d9fac5b632160f2615e1bbc0ec452
Wasa7shay80 <158795096+Wasa7shay80@users.noreply.github.com>
2025-05-04 15:42:12 +0530

    Update config.py

--- config.py
+++ config.py
@@ -26,7 +26,7 @@ CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "-1002324804439"))
 OWNER_ID = int(os.environ.get("OWNER_ID", "7040944963"))
 
 #Port
-PORT = os.environ.get("PORT", "1807")
+PORT = os.environ.get("PORT", "1808")
 
 #Database
 DB_URI = os.environ.get("DATABASE_URL", "mongodb+srv://Kuchupuchu:Akshay8080@kuchu.7li4w.mongodb.net/?retryWrites=true&w=majority")
@@ -34,8 +34,8 @@ JOIN_REQS_DB = environ.get("JOIN_REQS_DB", DB_URI)
 DB_NAME = os.environ.get("DATABASE_NAME", "Koi")
 
 #force sub channel id, if you want enable force sub
-FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", "-1001476290564"))
-FORCE_SUB_CHANNEL2 = int(os.environ.get("FORCE_SUB_CHANNEL2", "-1002571574162"))
+FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", "-1001313313247"))
+FORCE_SUB_CHANNEL2 = int(os.environ.get("FORCE_SUB_CHANNEL2", "-1002613964033"))
 
 TG_BOT_WORKERS = int(os.environ.get("TG_BOT_WORKERS", "4"))
 
